e48ba2f82bdd6bf3807bfddd8c6742569393e995
CodeHTE <2055964990@qq.com>
2026-05-13 16:49:02 +0800

    update toml and wsgi for healthcheck

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from app import app, socketio
+import os
 
+# Production entry point
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
